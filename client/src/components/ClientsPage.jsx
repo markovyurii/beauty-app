14e82a687860dfb036e895f5376dccb53a5a891d
yuriimarkov <yurko.markov@gmail.com>
2026-08-18 23:08:57 +0300

    build: implement auto reactive data re-fetching for price and clients views

--- client/src/components/ClientsPage.jsx
+++ client/src/components/ClientsPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function ClientsPage({ clients, expandedClientId, setExpandedClientId, handleUpdateClientNotes, handleDeleteClient,loadClientsData }) {
   useEffect(() => {
