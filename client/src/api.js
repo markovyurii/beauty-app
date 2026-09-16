477f5196c9ed3a7d74c0a3e26c0bf01c93068d35
yuriimarkov <yurko.markov@gmail.com>
2026-09-16 21:04:43 +0300

    Fix dynamic API URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,11 @@
-const API_URL = ''; // Порожній рядок, бо Vite використовує proxy. При деплої це полегшить життя.
+const API_URL =
+  typeof window !== 'undefined' &&
+  window.location.hostname.includes('netlify.app')
+    ? 'https://beauty-app-five-olive.vercel.app'
+    : ''; // Порожній рядок, бо Vite використовує proxy. При деплої це полегшить життя.
 export const API = {
   SERVICES: `${API_URL}/api/services`,
   CLIENTS: `${API_URL}/api/clients`,
   APPOINTMENTS: `${API_URL}/api/appointments`,
-  CALENDAR: `${API_URL}/api/calendar`
+  CALENDAR: `${API_URL}/api/calendar`,
 };
