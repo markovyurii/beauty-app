b568095daf5f932edad8bbae92f936980fe03f93
yuriimarkov <yurko.markov@gmail.com>
2026-09-16 22:17:16 +0300

    return good project

--- server/server.js
+++ server/server.js
@@ -28,12 +28,7 @@ app.use((req, res, next) => {
   }
   next();
 });
-app.use((req, res, next) => {
-  if (req.query.path) {
-    req.url = req.query.path;
-  }
-  next();
-});
+
 app.use(express.json());
 
 // ПІДКЛЮЧЕННЯ ДО БАЗИ ДАНИХ MONGODB
